3dc34510fb246658d155f0f7c22e4b7f4e8f5564
iftahmed <36085311+iftahmed@users.noreply.github.com>
2021-05-12 19:32:05 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -7,18 +7,35 @@ Created on Fri May  7 21:52:32 2021
 
 import pandas as pd
 import statsmodels.formula.api as smf
+import statsmodels.api as sm
 
-data = pd.read_csv('Portugual_hour_clean.csv')
+data_porto = pd.read_csv('Portugual_hour_clean.csv')
 
 
 #Linear model
 #C(x) indicates that x is a categorical variable 
-model = smf.ols('total_count ~ windspeed + temp + atemp + hum + C(season) + C(weathersit) + C(weekday) + C(year) + C(month) + C(hour)', data = data).fit() ## sm.OLS(output, input)
+model_porto = smf.ols('total_count ~ windspeed + temp + atemp + hum + C(season) + C(weathersit) + C(weekday) + C(month) + C(hour)', data = data_porto).fit() ## sm.OLS(output, input)
 
-model.summary()
+model_porto.summary()
 
 # Print out the statistics
 
+
+data_seoul = pd.read_csv('Seoul_hour_clean.csv')
+
+model_seoul = smf.ols('count ~ rainfall + solar_rad + dew_point + visibility + windspeed + hum + temp + C(weekday) + C(month) + C(hour) + C(season)', data = data_seoul).fit()
+
+model_seoul.summary()
+
+X_train = data_porto[['temp', 'atemp', 'hum', 'windspeed']]
+Y_train = data_porto[['total_count']]
+
+X = X_train
+y = Y_train
+
+model_temp_porto = smf.ols('total_count~ temp + atemp + hum + windspeed', data =data_porto).fit()
+
+model_temp_porto.summary()
 
 
 
